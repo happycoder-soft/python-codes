5f6672bc5c41c08130dd97ed09407f929068db84
happycoder-soft <happykumar1632@gmail.com>
2024-07-24 01:25:27 +0530

    add new files

---
+++ whatsapp.py
@@ -0,0 +1,11 @@
+def sum(a,b):
+    c=(a+b)
+    print(c)
+    
+import pyautogui as py
+import time 
+time.sleep(3)
+for i in range(100):
+    py.typewrite("thanku bol")
+    py.press("enter")
+   
