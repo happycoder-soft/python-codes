e70502d4b2761b8a469d314d3b30e480b07438a6
happycoder-soft <happykumar1632@gmail.com>
2025-01-07 13:26:15 +0530

    adding eye button in password section

--- LOGINSCREEN.py
+++ LOGINSCREEN.py
@@ -2,6 +2,7 @@ from tkinter import *
 from tkinter import font
 from tkinter import messagebox
 from tkinter.messagebox import showinfo , showerror
+import pymysql
 
 window=Tk()
 window.geometry("1100x600")
@@ -10,6 +11,8 @@ window.title("Login")
 window.iconbitmap(r"D:\user.ico.ico")
 window.config(bg="white")
 photo=PhotoImage(file="D:\login screen.png")
+
+
 
 def on_click(event):    
     tp1=Toplevel(window)
@@ -36,7 +39,7 @@ def clear_entry():
     entry_box.delete(0,END)
     entry_box1.delete(0,END)
 
-def Final():
+def connect_database():
     username1 = entry_box.get()
     password1 = entry_box1.get()
     print("id: {}".format(var.get())) 
@@ -46,6 +49,20 @@ def Final():
         messagebox.showinfo("Login Successful", "Welcome! Login Successful")
     else:
         messagebox.showerror("Login Failed", "Invalid username or password")
+        
+        
+def HIDE():
+    closeeye.config(file="D:\eye (1).png")
+    entry_box1.config(show="*")
+    eyebutton.config(command=show)
+    
+    
+def show():
+    closeeye.config(file="D:\hidden.png")
+    entry_box1.config(show="")
+    eyebutton.config(command=HIDE)
+    
+                            
 
 var=StringVar()
 var2=StringVar() 
@@ -77,10 +94,15 @@ register_here.place(x=830,y=519)
 entry_box=Entry(window,font=(30),textvariable=var,bd=3,relief="groove")
 entry_box.place(x=655,y=220,width=350,height=40)
 
-entry_box1=Entry(window,font=(30),textvariable=var2,bd=3,show="*",relief="groove")
+entry_box1=Entry(window,font=(30),textvariable=var2,bd=3,relief="groove")
 entry_box1.place(x=655,y=320,width=350,height=40)
 
-login_button=Button(window,text="LOGIN",font=(15),bg="#BBC6EE",relief="groove",command=Final)
+closeeye=PhotoImage(file="D:\hidden.png")
+eyebutton=Button(window,image=closeeye,bd=0,bg="white",activebackground="white",cursor="hand2",command=HIDE)
+eyebutton.place(x=980,y=330)
+
+
+login_button=Button(window,text="LOGIN",font=(15),bg="#BBC6EE",relief="groove",command=connect_database)
 login_button.place(x=680,y=430,height=40,width=100)
 
 clear_button=Button(window,text="CLEAR",font=(15),bg="#BBC6EE",relief="groove",command=clear_entry)
