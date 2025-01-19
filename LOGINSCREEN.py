4ec599e57a8c4b1949cbd993f034b3d93941b575
happycoder-soft <happykumar1632@gmail.com>
2025-01-19 22:33:08 +0530

    Few design

--- LOGINSCREEN.py
+++ LOGINSCREEN.py
@@ -4,27 +4,42 @@ from tkinter import messagebox
 from tkinter.messagebox import showinfo , showerror
 import pymysql
 
+
+
+#create window
 window=Tk()
+
+#window size
 window.geometry("1100x600")
 window.resizable(False,False)
+
+#window title
 window.title("Login")
+#window icon
 window.iconbitmap(r"D:\user.ico.ico")
+
+#window background color
 window.config(bg="white")
+
+#adding photo for use
 photo=PhotoImage(file="D:\login screen.png")
 
-
+#create function for open forget password window
 def forg():
     window.destroy()
     import forget
-    
+#create function for open registration window    
 def regi():
     window.destroy()
     import REGISTER
-    
+
+#create function for clear/delete all entry    
 def clear_entry():
     entry_box.delete(0,END)
     entry_box1.delete(0,END)
 
+
+#database connecting 
 def connect_database():
     # username1 = entry_box.get()
     # password1 = entry_box1.get()
@@ -56,13 +71,13 @@ def connect_database():
         messagebox.showinfo("Message","Login Successful")
         
         
-        
+#hide password button        
 def HIDE():
     closeeye.config(file="D:\eye (1).png")
     entry_box1.config(show="*")
     eyebutton.config(command=show)
     
-    
+#show password button     
 def show():
     closeeye.config(file="D:\hidden.png")
     entry_box1.config(show="")
@@ -74,51 +89,63 @@ def show():
 # var2=StringVar() 
  
 
-
+#adding image
 label=Label(window,image=photo,bg="white")
 label.place(x=30,y=30)
 
+
+#create login font
 login=Label(window,text="Login",font=('Microsoft YaHei UI Light',40),bg="white")
 login.place(x=780,y=80)
+#create username font
 username=Label(window,text="USERNAME",font=('Microsoft YaHei UI Light',13),bg="white")
 username.place(x=650,y=190)
 
-
+#create password font
 password=Label(window,text="PASSWORD",font=('Microsoft YaHei UI Light',13),bg="white")
 password.place(x=650,y=290)
 
+#adding forget password button font
 forget_password=Button(window,text="Forget Password?",font=('Microsoft YaHei UI Light',10),bg="white",cursor="hand2",relief="flat",command=forg)
 forget_password.place(x=890,y=355)
 
+
+#create new user font
 new_user=Label(window,text="New User ?",font=('Microsoft YaHei UI Light',10),bg="white")
 new_user.place(x=760,y=520)
 
+
+#adding register here button font 
 register_here=Button(window,text="Register Here",font=('Microsoft YaHei UI Light',10),bg="white",
                      activebackground="white",activeforeground="black",cursor="hand2",command=regi,relief="flat")
 register_here.place(x=832,y=516)
 
-
+#adding username entry box
 entry_box=Entry(window,font=(30),bd=3,relief="groove")
 entry_box.place(x=655,y=220,width=350,height=40)
 
+#adding password entry box
 entry_box1=Entry(window,font=(30),bd=3,relief="groove")
 entry_box1.place(x=655,y=320,width=350,height=40)
 
+#adding show and hide photo and button
 closeeye=PhotoImage(file="D:\hidden.png")
 eyebutton=Button(window,image=closeeye,bd=0,bg="white",activebackground="white",cursor="hand2",command=HIDE)
 eyebutton.place(x=980,y=330)
 
-
+#create login button
 login_button=Button(window,text="LOGIN",font=(15),bg="#BBC6EE",relief="groove",command=connect_database)
 login_button.place(x=680,y=430,height=40,width=100)
 
+
+#adding clear button
 clear_button=Button(window,text="CLEAR",font=(15),bg="#BBC6EE",relief="groove",command=clear_entry)
 clear_button.place(x=880,y=430,height=40,width=100)
 
-custom_font = font.Font(family="Helvetica", size=12)
 
 
 # forget_password.bind("<Button-1>", on_click)
 # register_here.bind("<Button-1>", register)
 
+#window running loop
 window.mainloop()
--- forget.py
+++ forget.py
@@ -5,15 +5,19 @@ import pymysql
 
 
 
-
+#create full windows setup
 window=Tk()
 window.geometry("1050x550")
 window.iconbitmap(r"D:\user.ico.ico")
 window.title("Forget Password")
 window.resizable(FALSE,FALSE)
 window.config(bg="white")
+
+#adding photo for later use
 photo=PhotoImage(file="D:\coding\icons\pic.png")
 
+
+#function for save button
 def save():
     if username_entrybox.get()=="" or new_password_entrybox.get() =="":
        messagebox.showerror("Error","All Fields Are Required!")
@@ -33,42 +37,53 @@ def save():
            messagebox.showinfo("Message","Password is successfully changed")
            window.destroy()     
            import LOGINSCREEN
-      
+ 
+#function for login button      
 def log():
      window.destroy()
      import LOGINSCREEN
 
+#create function for clear button
 def all_del():
     username_entrybox.delete(0,END)
     new_password_entrybox.delete(0,END)
 
-
+#adding image
 pic=Label(window,image=photo,bg="white")
 pic.place(x=700,y=160)
 
+
+#create lable on upper side
 head=Label(window,text="FORGET PASSWORD",fg="black",font=("calibri",30),background="white")
 head.place(x=150,y=60)
 
+#create username font
 username=Label(window,text="USERNAME          :",font=("Microsoft YaHei UI Light",20),background="white",fg="black")
 username.place(x=70,y=170)
 
+#create new password font
 new_password=Label(window,text="NEW PASSWORD :",font=("Microsoft YaHei UI Light",20),background="white",fg="black")
 new_password.place(x=70,y=270)
 
+#create username entry box
 username_entrybox=Entry(window,font=("calibri",15),background="#85b4ff",fg="black",relief="sunken")
 username_entrybox.place(x=320,y=180,height=30,width=250)
 
+#create new password entry box
 new_password_entrybox=Entry(window,font=("calibri",15),background="#85b4ff",fg="black",relief="sunken")
 new_password_entrybox.place(x=320,y=277,height=30,width=250)
 
+#create save button 
 save=Button(window,text="SAVE",font=("calibri",15),background="#a3faff",fg="black",command=save)
 save.place(x=110,y=400,height=35,width=100)
 
+#create login button
 login=Button(window,text="LOGIN",font=("calibri",15),background="#a3faff",fg="black",command=log)
 login.place(x=270,y=400,height=35,width=100)
 
+#create login button
 clear=Button(window,text="CLEAR",font=("calibri",15),background="#a3faff",fg="black",command=all_del)
 clear.place(x=430,y=400,height=35,width=100)
 
-
+#and last here is window loop for running
 window.mainloop()
