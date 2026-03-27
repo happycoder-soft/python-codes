4294f796b80021f5568fd97a77dc3d3b303c2570
happycoder-soft <happykumar1632@gmail.com>
2026-03-27 23:29:50 +0530

    pandas day

--- pandas/01.py
+++ pandas/01.py
@@ -1,11 +1,13 @@
+from hashlib import new
+
 import pandas as pd
 import openpyxl
 
 # Create a sample DataFrame using a dictionary
 data = {
     'name':['happy','sachin','shivam','aditya','abhishek'],
-    'age':[20,26,24,21,22],
-    'city':['Motihari','Darbhanga',None,'Hajipur','Khagaria'],
+    'age':[20,26,24,24,22],
+    'city':['Motihari','Darbhanga', 'Darbhanga','Hajipur','Khagaria'],
     'salary':[100000,60000,55000,45000,80000]
     } 
 
@@ -91,13 +93,79 @@ df=pd.DataFrame(data)
 # #detect missing values
 # print(df.isnull())
 
-#handle missing values by filling with a specific value
-df['city'].fillna('Unknown', inplace=True)
-print(df)   
+# #handle missing values by filling with a specific value
+# df['city'].fillna('Unknown', inplace=True)
+# print(df)   
 
 # #handle missing values by dropping(deleting) rows with missing values
 # df.dropna(inplace=True)
 # print(df)
+
+
+# # #handle missing values by filling with mean value
+# df['age'].fillna(df['age'].mean(), inplace=True)
+# print(df)
+
+# #interpolate missing values
+# df['age']=df['age'].interpolate(method='linear')
+# print(df)
+
+# #time based interpolation
+# df['age']=df['age'].interpolate(method='time')
+# print(df)
+
+#polynomial interpolation
+# df = pd.DataFrame({
+#     'x': [1, 2, 3, 4, 5,6,7,8,9],
+#     'y': [1, 8, 27, None, 125, 216, 343, None, 729]
+# })
+
+# df['y'] = df['y'].interpolate(method='polynomial', order=3)#order 3 means(3 value ke bad none value hona chahiye) cubic interpolation or 2 means quadratic interpolation
+
+# print(df)
+
+# #resampling time series data
+# date_rng = pd.date_range(start='2024-01-01', end='2024-01-10', freq='D')
+# df = pd.DataFrame(date_rng, columns=['date'])
+# df['data'] = pd.Series(range(len(df)))
+# print(df)
+
+
+#forward fill
+
+# df = pd.DataFrame({
+#     'A': [10, None, None, 40, None]
+# })
+
+# new=df.fillna() #forward fill method se missing value ko pichle value se fill karte hai
+# print(new)
+
+# #backward fill
+# df = pd.DataFrame({
+#       'A': [10, None, None, 40, None]
+# })
+# new=df.fillna(method='bfill') #backward fill method se missing value ko agle value se fill karte hai
+# print(new)
+
+# #sorting data
+# w=df.sort_values(by='age', ascending=True) #age ke hisab se data ko descending order me sort karte hai
+# print(w)
+#ascending ko true or false karenge to ascending or descending order me sort karte hai
+
+# #multiple column ke hisab se sort karte hai
+# r=df.sort_values(by=['age','salary','city'],ascending=[True,True,True]) #age ke hisab se ascending order me sort karte hai aur salary ke hisab se descending order me sort karte hai
+# print(r)
+
+#grouping data
+
+
+
+
+
+
+
+
+
 
 
 
